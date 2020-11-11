627c47b50a311bf69da1cce6891264d5de5d8223
Simon Woodworth <s.woodworth@ucc.ie>
2020-11-11 14:30:11 +0000

    Changed hello message

--- src/main/java/com/simonwoodworth/is3313minimaljavademo/Main.java
+++ src/main/java/com/simonwoodworth/is3313minimaljavademo/Main.java
@@ -12,7 +12,7 @@ package com.simonwoodworth.is3313minimaljavademo;
 public class Main {
     
     public static void main (String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello IS3313");
     }
     
 }
